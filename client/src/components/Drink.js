9dd15d21c160d1b397e231c0e043ec8943f8450f
PJ Frias <PeejFrias@Gmail.com>
2018-04-07 13:49:44 -0400

    start show rendering component of drink

--- client/src/components/Drink.js
+++ client/src/components/Drink.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-
 export const Drink = (props) => {
+
+  const ingredients = props.drink.ingredients.map((ingredient, i) => {
+    return <li key={i}>{ingredient.quantity} {ingredient.item.name}</li>
+  })
+
   return (
     <div>
       <h3>Random Drink {props.number}</h3>
+      <h2>{props.drink.name}</h2>
+      <p>{props.drink.category} - <em>({props.drink.glass})</em></p>
+      <strong>Ingredients:</strong>
+      <ul>{ingredients}</ul>
+      <strong>Instructions:</strong>
+      <p>{props.drink.instructions}</p>
+
+      <img src={props.drink.img_url} />
     </div>
   )
 }
-
-    
-
--- client/src/containers/DrinkShow.js
+++ client/src/containers/DrinkShow.js
@@ -2,6 +2,38 @@ import React from 'react';
 import { Drink } from '../components/Drink'
 
 
+
+const loadDrink = {
+  id: 1,
+  name: "'57 Chevy with a White License Plate",
+  category: "Cocktail",
+  glass: "Highball glass",
+  img_url: "https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg",
+  instructions: "1. Fill a rocks glass with ice 2.add white creme de cacao and vodka 3.stir",
+  ingredients: [
+    {
+      id: 1,
+      quantity: "1 oz white ",
+      item: {
+        id: 1,
+        name: "Creme de Cacao",
+        created_at: "2018-04-07T15:12:29.890Z",
+        updated_at: "2018-04-07T15:12:29.890Z"
+      }
+    },
+    {
+      id: 2,
+      quantity: "1 oz ",
+      item: {
+        id: 2,
+        name: "Vodka",
+        created_at: "2018-04-07T15:12:30.100Z",
+        updated_at: "2018-04-07T15:12:30.100Z"
+      }
+    }
+  ]
+}
+
 export default class DrinkShow extends React.Component {
   constructor(props){
     super(props)
@@ -24,7 +56,7 @@ export default class DrinkShow extends React.Component {
         <p>Will display all info of a drink</p>
         <button onClick={this.handleOnClick}>Load New Drink</button>
         <hr/>
-        <Drink number={this.state.number}/>
+        <Drink number={this.state.number} drink={loadDrink}/>
       </div>
     )
   }
