879aa27a9c69ff63907c201a6b09a5bab133494c
PJ Frias <PeejFrias@Gmail.com>
2018-04-08 14:39:26 -0400

    import react router and organize notes files into folder

---
+++ NOTES/MYBAR_PLANNING.md
@@ -0,0 +1,24 @@
+const Bar_Essentials = {
+  Liquors: [Gin,
+    Dark Rum
+    Light Rum
+    Tequila,
+    Brandy,
+    Bourbon,
+    Rye Whiskey,
+    Vodka],
+  Liquers: [
+    Sweet Vermouth,
+    Dry Vermouth,
+    Amaretto,
+    Coffee Liqueur,
+    Orange Liqueur
+  ],
+  Mixers & Garnishes: [
+    Club Soda,
+    Tonic,
+    Ginger ale,
+    Bitters
+  ]
+}
+  
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 // import logo from './logo.svg';
 import './App.css';
 
@@ -9,32 +11,28 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
+      <Router>
+        <div>
+          <nav className="blue-grey darken-4">
+            <div className="nav-wrapper">
+              <a href="/" className="brand-logo">Concoctail</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+              </ul>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="col s-12">
+              <Route exact path="/drinks" component={DrinkShow} />
+              {/* <DrinkIndex /> */}
+            </div>
+          </div>
 
-        
-        <nav className="blue-grey darken-4">
-          <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Concoctail</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">JavaScript</a></li>
-            </ul>
-          </div>
-        </nav>
-        <div className="container">
-          <div className="col s-12">
-            <DrinkShow />
-            {/* <DrinkIndex /> */}
-          </div>
+          
         </div>
-
-        
-      </div>
+      </Router>
     );
   }
 }
